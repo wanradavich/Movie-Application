54891a793ea941bcee5facf4b63d850a055ca279
ezhangg <eunicezh10@gmail.com>
2024-01-12 21:59:27 -0800

    bootstrap

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import HomeCategories from "../components/HomeCategories";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function HomePage() {
   const [popularMovies, setPopularMovies] = useState([]);
