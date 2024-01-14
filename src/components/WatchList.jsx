3de159c8e327dbd3ea5f8d4b163c537ae27268ed
ezhangg <eunicezh10@gmail.com>
2024-01-13 23:43:29 -0800

    change watchlist logo

--- src/components/WatchList.jsx
+++ src/components/WatchList.jsx
@@ -6,16 +6,13 @@ const WatchList = ({ movie, onClick }) => {
     return (
       <div className="watch-list-container" onClick={handleWatchListClick}>
         <span className="mr-2 text-white"></span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="30"
-          height="30"
-          className="bi bi-patch-plus-fill"
-          viewBox="0 0 16 16"
-        >
-          <path
-            d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01zM8.5 6v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 1 0"
-          />
+        <svg xmlns="http://www.w3.org/2000/svg" 
+            width="30" 
+            height="30" 
+            fill="currentColor" 
+            className="bi bi-plus-square-fill" 
+            viewBox="0 0 16 16">
+            <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0"/>
         </svg>
       </div>
     );
