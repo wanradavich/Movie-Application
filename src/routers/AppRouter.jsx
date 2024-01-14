158102a351965a5381f560245070d7864f3cfcaf
Wanrada Pat <wrda.ruks@gmail.com>
2024-01-13 17:19:31 -0800

    about page styled mobile first

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -4,6 +4,8 @@ import TopRated from "../pages/TopRated";
 import NowPlaying from "../pages/NowPlaying";
 import Upcoming from "../pages/Upcoming";
 import Navbar from "../components/Navbar";
+import AboutPage from "../pages/AboutPage";
+import Footer from "../components/Footer";
 
 function AppRouter() {
   return (
@@ -12,10 +14,12 @@ function AppRouter() {
         <Navbar />
         <Routes>
           <Route path="/" exact element={<HomePage />} />
+          <Route path="/about" exact element={<AboutPage />} />
           <Route path="/toprated" exact element={<TopRated />} />
           <Route path="/nowplaying" exact element={<NowPlaying />} />
           <Route path="/upcoming" exact element={<Upcoming />} />
         </Routes>
+        <Footer />
       </div>
     </BrowserRouter>
   );
