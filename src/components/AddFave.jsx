487ad0ecb735908603dd5956af4b5a8e11ed1a30
ezhangg <eunicezh10@gmail.com>
2024-01-14 01:01:53 -0800

    clean comments

--- src/components/AddFave.jsx
+++ src/components/AddFave.jsx
@@ -1,82 +1,3 @@
-// import { useEffect, useState } from 'react';
-
-// const AddFave = ({ movie, onClick }) => {
-//   const [isInFaveList, setIsInFaveList] = useState(false);
-
-//   useEffect(() => {
-//     const faveMovies = JSON.parse(localStorage.getItem('faveMovies')) || [];
-//     setIsInFaveList(faveMovies.some((faveMovie) => faveMovie.id === movie.id));
-//   }, [movie.id]);
-
-//   const handleFaveClick = () => {
-//     onClick(movie);
-//     setIsInFaveList(!isInFaveList);
-//   };
-
-//   return (
-//     <div
-//       className={`heart-container ${isInFaveList ? 'active' : ''}`}
-//       onClick={handleFaveClick}
-//     >
-//       <span className="mr-2 text-white"></span>
-//       <svg
-//         xmlns="http://www.w3.org/2000/svg"
-//         width="30"
-//         height="30"
-//         className={`bi bi-heart${isInFaveList ? '-fill' : ''}`}
-//         viewBox="0 0 16 16"
-//       >
-//         <path
-//           fillRule="evenodd"
-//           d="M7.375 14.5s-1.5 1.5-2.375 1.5C4.5 16 4 15 4 15s-1.5 1-1.625 1.5c-.108.414-.625 2.5 2.625 2.5 3.25 0 2.75-2.25 2.75-2.5 0-.25-.5-1.5-.5-1.5zM1 7c0-3.5 2.5-6 6-6 3.148 0 5.5 2.5 5.5 6 0 4-6 9-6 9s-6-5-6-9z"
-//         />
-//       </svg>
-//     </div>
-//   );
-// };
-
-// export default AddFave;
-
-
-
-// const AddFave = ({ movie, onClick }) => {
-    
-//   const isMovieInFaveList = () => {
-//     const faveMovies = JSON.parse(localStorage.getItem('faveMovies')) || [];
-//     return faveMovies.some((faveMovie) => faveMovie.id === movie.id);
-//   };
-
-//   const handleFaveClick = () => {
-//     onClick(movie);
-//   };
-
-//   return (
-//     <div
-//       className={`heart-container ${isMovieInFaveList() ? 'active' : ''}`}
-//       onClick={handleFaveClick}
-//     >
-//       <span className="mr-2 text-white"></span>
-//       <svg
-//         xmlns="http://www.w3.org/2000/svg"
-//         width="30"
-//         height="30"
-//         className={`bi bi-heart${isMovieInFaveList() ? '-fill' : ''}`}
-//         viewBox="0 0 16 16"
-//       >
-//         <path
-//           fillRule="evenodd"
-//           d="M7.375 14.5s-1.5 1.5-2.375 1.5C4.5 16 4 15 4 15s-1.5 1-1.625 1.5c-.108.414-.625 2.5 2.625 2.5 3.25 0 2.75-2.25 2.75-2.5 0-.25-.5-1.5-.5-1.5zM1 7c0-3.5 2.5-6 6-6 3.148 0 5.5 2.5 5.5 6 0 4-6 9-6 9s-6-5-6-9z"
-//         />
-//       </svg>
-//     </div>
-//   );
-// };
-
-// export default AddFave;
-
-////////////////////////////////////////////////////////////////////////
-
-
 const AddFave = ({ movie, onClick }) => {
 
     const handleFaveClick = () => {
@@ -105,5 +26,5 @@ const AddFave = ({ movie, onClick }) => {
 export default AddFave;
 
 
-////////////////
+
 
