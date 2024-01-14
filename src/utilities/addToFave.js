76f93df5efe68b221bd9b0a2d7720283fc9bac0e
ezhangg <eunicezh10@gmail.com>
2024-01-13 21:56:59 -0800

    can remove from list now

--- src/utilities/addToFave.js
+++ src/utilities/addToFave.js
@@ -1,9 +1,30 @@
+// export const addToFave = (movie) => {
+//     const faveMovies =
+//       JSON.parse(localStorage.getItem("faveMovies")) || [];
+  
+//     if (!faveMovies.some((faveMovie) => faveMovie.id === movie.id)) {
+//       faveMovies.push(movie);
+//       localStorage.setItem("faveMovies", JSON.stringify(faveMovies));
+//     }
+//   };
+
+
 export const addToFave = (movie) => {
     const faveMovies =
       JSON.parse(localStorage.getItem("faveMovies")) || [];
   
-    if (!faveMovies.some((faveMovie) => faveMovie.id === movie.id)) {
+    const movieIndex = faveMovies.findIndex(
+      (faveMovie) => faveMovie.id === movie.id
+    );
+  
+    if (movieIndex !== -1) {
+      // If the movie is already in the favorites, remove it
+      faveMovies.splice(movieIndex, 1);
+    } else {
+      // If the movie is not in the favorites, add it
       faveMovies.push(movie);
-      localStorage.setItem("faveMovies", JSON.stringify(faveMovies));
     }
+  
+    localStorage.setItem("faveMovies", JSON.stringify(faveMovies));
   };
+  
