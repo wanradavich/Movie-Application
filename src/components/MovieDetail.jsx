6397b29d4b479b95f7a18f74577c434e76500acf
Sydnee Snowball <s.snowball@hotmail.com>
2024-02-19 17:05:31 -0800

    more details

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -35,42 +35,49 @@ const MovieDetails = () => {
 
   return (
     <div className="movie-individual">
-      <h2 id="movie-detail-title">Movie Details</h2>
-      <img
-        src={`${baseImageUrl}${movie.poster_path}`}
-        alt={movie.title}
-        style={{ width: "200px", height: "300px" }}
-      />
-      <div className="movie-details-container">
-        <h3>{movie.title}</h3>
-        <div id="overview">
-            <h5>Overview</h5>
-            <p>{movie.overview}</p>
-        </div>
-        <div id="released">
-            <h5>Released</h5>
-            <p>{movie.release_date}</p>
-        </div>
-        <div id="rating">
-            <h5>Rating</h5>
-            <p>{movie.vote_average.toFixed(1)}/10</p>
-        </div>
-        <div id="genre">
+      {/* <h2 id="movie-detail-title">Movie Details</h2> */}
+      <div>
+        <img
+          src={`${baseImageUrl}${movie.poster_path}`}
+          alt={movie.title}
+          style={{ width: "200px", height: "300px" }}
+        />
+        <div className="movie-details-container">
+          <h3>{movie.title}</h3>
+          <div id="overview">
+              <h5>Overview</h5>
+              <p>{movie.overview}</p>
+          </div>
+          <div id="released">
+              <h5>Released</h5>
+              <p>{movie.release_date}</p>
+          </div>
+          <div id="rating">
+              <h5>Rating</h5>
+              {movie.vote_average !== undefined ? (
+                  <p>{movie.vote_average.toFixed(1)}/10</p>
+              ) : (
+                  <p>N/A</p>
+              )}
+          </div>
+          <div id="genre">
             <h5>Genre</h5>
             <p>
-                {movie.genres && movie.genres.map((genre) => (
-                    <span key={genre.id}>{genre.name}, </span>
+                {movie.genres && movie.genres.map((genre, index) => (
+                    <span key={genre.id}>
+                        {genre.name}
+                        {index !== movie.genres.length - 1 && ", "}
+                    </span>
                 ))}
             </p>
+          </div>
+          <Link to={`/`}>
+            <button>Back to List</button>
+          </Link>
         </div>
       </div>
-      <Link to={`/`}>
-        <button>Back to List</button>
-      </Link>
-
       {video && (
         <div>
-          <p>Video:</p>
           <iframe
             width="560"
             height="315"
@@ -83,85 +90,3 @@ const MovieDetails = () => {
 };
 
 export default MovieDetails;
-
-// //import { Link} from "react-router-dom";
-// import { addToFavorites, removeFromFavorites } from "../slices/favoritesSlice";
-// import { useDispatch } from "react-redux";
-
-// function MovieDetail () {
-//     const apiKey = "d54e5d8cf2227762d2ed37b16b4ea050";
-//     const baseImageUrl = "https://image.tmdb.org/t/p/w500";
-//     //const dispatch = useDispatch();
-
-//     // function handleToggleFavorite() {
-//     //     if (addToFavorites === true) {
-//     //         console.log(movieObj);
-//     //         dispatch(addToFavorites(movieObj));
-//     //     }
-//     //     else {
-//     //         dispatch(removeFromFavorites(movieObj));
-//     //     }
-//     // }
-
-//     return (
-//         <div>
-//             {isFavourite && (
-//                 <div>
-//                     <button onClick={handleToggleFavorite}>
-//                         heart
-//                     </button>
-//                     {/* this is where the heart would go for favourite */}
-//                 </div>
-//             )}
-//             <div className="movie-profile-photo">
-//                 <img
-//                     src={`${baseImageUrl}${movieObj.poster_path}`}
-//                     alt={movieObj.title}
-//                 />
-//             </div>
-//             <div className="movie-info">
-//                 <h3 id="movie-title">
-//                     {movieObj.title}
-//                 </h3>
-//                 <div id="overview">
-//                     <h5>
-//                         Overview:
-//                     </h5>
-//                     <p>
-//                         {movieObj.overview}
-//                     </p>
-//                 </div>
-//                 <div id="released">
-//                     <h5>
-//                         Released:
-//                     </h5>
-//                     <p>
-//                         {movieObj.release_date}
-//                     </p>
-//                 </div>
-//                 <div id="rate">
-//                     <h5>
-//                         Rating:
-//                     </h5>
-//                     <p>
-//                         {movieObj.vote_average}
-//                     </p>
-//                 </div>
-//                 <div id="genre">
-//                     <h5>
-//                         Genre:
-//                     </h5>
-//                     <p>
-//                         {movieObj.genres.map((genre) => (
-//                             <span key={genre.id}>{genre.name}</span>
-//                         ))}
-//                     </p>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// } 
-// // MovieDetail.defaultProps = {
-// //     movieLink: true,
-// //   };
-// export default MovieDetail;
