91e9fd6752cdf39bc01c9205e1c7c430f42b44e5
Wanrada Pat <wrda.ruks@gmail.com>
2024-02-23 19:46:31 -0800

    updating upcoming movies to be up to date

--- src/pages/Upcoming.jsx
+++ src/pages/Upcoming.jsx
@@ -2,9 +2,12 @@ import MovieCategory from "../components/MovieCategory";
 
 const TopRated = () => {
   const apiKey = import.meta.env.VITE_API_KEY;
-  const upcoming = "https://api.themoviedb.org/3/movie/upcoming";
+  const currentDate = new Date().toISOString().split("T")[0]; // Get current date in YYYY-MM-DD format
 
-  const upcomingApiUrl = `${upcoming}?api_key=${apiKey}`;
+  // Construct the API URL with additional query parameters
+  const upcoming = "https://api.themoviedb.org/3/movie/upcoming";
+  const upcomingApiUrl = `${upcoming}?api_key=${apiKey}&primary_release_date-gte=${currentDate}&region=US`;
+
   return <MovieCategory apiUrl={upcomingApiUrl} />;
 };
 
